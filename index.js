7189ae0e794c8083a2784192746e7b75a46bdfb7
Bablu Kumar Singh <bksingh3520@gmail.com>
2023-04-13 10:13:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var typeData = new Typed(".role",{
+    strings: [
+        "Front-End Developer",
+        "Back-End Developer",
+        "Full-Stack Developer",
+        "Ittu Sa Developer"
+    ],
+    loop: true,
+    typeSpeed:100,
+    backSpeed:80,
+    backDelay:1000, 
+
+});
